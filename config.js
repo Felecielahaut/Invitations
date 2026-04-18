a3cd30f0406a540fff752d11c9efd7793ea8e3fb
BlushMoments <felecielahaut@gmail.com>
2026-04-18 14:22:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const weddingConfig = {
+    // Names and Initials
+    partner1: "Laila",
+    partner2: "Kareem",
+    initial1: "L",
+    initial2: "K",
+    
+    // Dates
+    eventDateDisplay: "May 2nd, 2026",
+    eventDateShort: "May 2, 2026",
+    eventTime: "6:00 PM",
+    countdownTarget: "2026-05-02T18:00:00",
+    rsvpDeadline: "April 1st, 2026",
+    
+    // Location Details
+    venueName: "Armed Forces Hotel",
+    venueAddress: "Al Arish, North Sinai",
+    mapLink: "https://maps.app.goo.gl/rpKD4vcebtujBz3L6",
+    mapIframeSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.6!2d33.8244799!3d31.1429605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14fdd31134d7ce7b%3A0x67063f8471e355ee!5e0!3m2!1sar!2seg!4v1680193165242!5m2!1sar!2seg"
+};
