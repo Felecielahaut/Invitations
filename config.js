ab76ae15a98f3ae9807e278c40f7541300368f94
BlushMoments <felecielahaut@gmail.com>
2026-04-18 15:05:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,4 @@ const weddingConfig = {
     venueName: "The Nile Ritz-Carlton",
     venueAddress: "Cairo",
     mapLink: "https://maps.app.goo.gl/kU5WCxcxVtvRFWQh6",
-    mapIframeSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.6!2d33.8244799!3d31.1429605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14fdd31134d7ce7b%3A0x67063f8471e355ee!5e0!3m2!1sar!2seg!4v1680193165242!5m2!1sar!2seg"
-};
+mapIframeSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3411.6!2d33.8244799!3d31.1429605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14fdd31134d7ce7b%3A0x67063f8471e355ee!2z2YHZhtYK2YIg2KfZhNmC2YjYp9TSINK4!5e0!3m2!1sar!2seg!4v1680193165242!5m2!1sar!2seg"};
